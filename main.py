7662347873145de8ff8fc1bc08034799d2cac572
Pedro Paulo Dantas Franco Rocha <pedro.rocha@csjsistemas.com.br>
2025-02-10 16:50:31 -0300

    Ajuste main

--- main.py
+++ main.py
@@ -7,12 +7,6 @@ from handlers.agendamento import agendamento_handlers
 from handlers.visualizar import visualizar_handlers
 from flask import Flask
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Bot está rodando!"
-
 bot = telebot.TeleBot(TOKEN)
 
 # Registrar handlers
@@ -20,15 +14,5 @@ start(bot)
 agendamento_handlers(bot)
 visualizar_handlers(bot)
 
-def run_bot():
-    print("Bot está rodando...")
-    bot.infinity_polling()  # Usa infinity_polling() para evitar falhas
-
-# Iniciar o bot em uma thread separada
-bot_thread = threading.Thread(target=run_bot, daemon=True)
-bot_thread.start()
-
-# Iniciar o servidor Flask
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+print("Bot está rodando...")
+bot.infinity_polling()
